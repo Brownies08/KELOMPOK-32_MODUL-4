b9fad9b6ebada8bac41c01caad526ce4716bbd23
Hasnaa Amalia Q <164715987+hasnnaaa@users.noreply.github.com>
2024-03-29 19:01:28 +0700

    Update tugasmodul4.java

--- tugasmodul4.java
+++ tugasmodul4.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 
 public class tugasmodul4 {
-
+        
         static int saldo = 0;
-       static Scanner scanner = new Scanner(System.in);
+        static Scanner scanner = new Scanner(System.in);
 
         static int beliminuman(int harga) {
             if (saldo >= harga) {
